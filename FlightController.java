9d16cf344c520139a1e9287c6e1cc695cdfc91ad
Paarth Chandan <135144621+Paarth-Chandan@users.noreply.github.com>
2026-08-04 22:22:53 +0530

    Implement FlightController with search and view methods

---
+++ FlightController.java
@@ -0,0 +1,50 @@
+package com.flightreservation.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import com.flightreservation.model.Flight;
+import com.flightreservation.service.FlightService;
+
+@Controller
+@RequestMapping("/flight")
+public class FlightController {
+
+    @Autowired
+    private FlightService flightService;
+
+    @GetMapping("/search")
+    public String searchPage() {
+
+        return "searchFlight";
+
+    }
+
+    @PostMapping("/search")
+    public String search(@RequestParam String source,
+                         @RequestParam String destination,
+                         Model model) {
+
+        List<Flight> flights =
+                flightService.searchFlights(source, destination);
+
+        model.addAttribute("flights", flights);
+
+        return "searchFlight";
+
+    }
+
+    @GetMapping("/view")
+    public String viewFlights(Model model) {
+
+        model.addAttribute("flights",
+                flightService.getAllFlights());
+
+        return "viewFlights";
+
+    }
+
+}
