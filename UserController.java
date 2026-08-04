95060638a999714dc73c933cb89da5c817eb8510
Paarth Chandan <135144621+Paarth-Chandan@users.noreply.github.com>
2026-08-04 22:22:26 +0530

    Create UserController.java

---
+++ UserController.java
@@ -0,0 +1,67 @@
+package com.flightreservation.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import com.flightreservation.model.User;
+import com.flightreservation.service.UserService;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/signup")
+    public String signupPage(Model model) {
+
+        model.addAttribute("user", new User());
+
+        return "signup";
+    }
+
+    @PostMapping("/register")
+    public String register(@Valid @ModelAttribute User user,
+                           BindingResult result) {
+
+        if (result.hasErrors()) {
+            return "signup";
+        }
+
+        userService.registerUser(user);
+
+        return "login";
+    }
+
+    @GetMapping("/login")
+    public String loginPage() {
+
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam String email,
+                        @RequestParam String password,
+                        Model model) {
+
+        User user = userService.login(email, password);
+
+        if (user == null) {
+
+            model.addAttribute("error", "Invalid Credentials");
+
+            return "login";
+
+        }
+
+        return "userDashboard";
+
+    }
+
+}
