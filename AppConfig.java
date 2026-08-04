8e5924b3ae0306be59a00c942984e26d3319cc01
Paarth Chandan <135144621+Paarth-Chandan@users.noreply.github.com>
2026-08-04 21:59:54 +0530

    Create AppConfig.java

---
+++ AppConfig.java
@@ -0,0 +1,10 @@
+package com.flightreservation.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.flightreservation")
+public class AppConfig {
+
+}
