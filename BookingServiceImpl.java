71e2278950e0eb51ac4f07a858e719155f497240
Paarth Chandan <135144621+Paarth-Chandan@users.noreply.github.com>
2026-08-04 22:18:08 +0530

    Create BookingServiceImpl.java

---
+++ BookingServiceImpl.java
@@ -0,0 +1,38 @@
+package com.flightreservation.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.flightreservation.dao.BookingDao;
+import com.flightreservation.model.Booking;
+
+@Service
+public class BookingServiceImpl implements BookingService {
+
+    @Autowired
+    private BookingDao bookingDao;
+
+    @Override
+    public void addBooking(Booking booking) {
+
+        bookingDao.addBooking(booking);
+
+    }
+
+    @Override
+    public List<Booking> getBookingsByUser(int userId) {
+
+        return bookingDao.getBookingsByUser(userId);
+
+    }
+
+    @Override
+    public void cancelBooking(int bookingId) {
+
+        bookingDao.cancelBooking(bookingId);
+
+    }
+
+}
