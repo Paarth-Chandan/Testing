78cf63c8441f4583d72de7e6db8d2fb7d6b57e4f
Paarth Chandan <135144621+Paarth-Chandan@users.noreply.github.com>
2026-08-05 07:14:12 +0530

    Update BookingService.java

--- BookingService.java
+++ BookingService.java
@@ -6,7 +6,7 @@ import com.flightreservation.model.Booking;
 
 public interface BookingService {
 
-    void addBooking(Booking booking);
+    int addBooking(Booking booking);
 
     List<Booking> getBookingsByUser(int userId);
 
