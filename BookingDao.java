378a1ff7d1ceb474152820cd278c1ce6eabce9f9
Paarth Chandan <135144621+Paarth-Chandan@users.noreply.github.com>
2026-08-05 07:13:41 +0530

    Update BookingDao.java

--- BookingDao.java
+++ BookingDao.java
@@ -6,7 +6,7 @@ import com.flightreservation.model.Booking;
 
 public interface BookingDao {
 
-    void addBooking(Booking booking);
+    int addBooking(Booking booking);
 
     List<Booking> getBookingsByUser(int userId);
 
