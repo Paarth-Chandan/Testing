e84644f598fd8a75724f5c00a95d5475a60cff06
Paarth Chandan <135144621+Paarth-Chandan@users.noreply.github.com>
2026-08-05 07:13:56 +0530

    Update BookingDaoImpl.java

--- BookingDaoImpl.java
+++ BookingDaoImpl.java
@@ -1,10 +1,13 @@
 package com.flightreservation.dao;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.*;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import com.flightreservation.model.Booking;
@@ -16,19 +19,30 @@ public class BookingDaoImpl implements BookingDao {
     private JdbcTemplate jdbcTemplate;
 
     @Override
-    public void addBooking(Booking booking) {
+    public int addBooking(Booking booking) {
 
         String sql = "INSERT INTO booking(booking_date,journey_date,seats,total_price,status,user_id,flight_id) VALUES(?,?,?,?,?,?,?)";
 
-        jdbcTemplate.update(sql,
+        KeyHolder keyHolder = new GeneratedKeyHolder();
 
-                booking.getBookingDate(),
-                booking.getJourneyDate(),
-                booking.getSeats(),
-                booking.getTotalPrice(),
-                booking.getStatus(),
-                booking.getUserId(),
-                booking.getFlightId());
+        jdbcTemplate.update(connection -> {
+
+            PreparedStatement ps = connection.prepareStatement(sql,
+                    Statement.RETURN_GENERATED_KEYS);
+
+            ps.setString(1, booking.getBookingDate());
+            ps.setString(2, booking.getJourneyDate());
+            ps.setInt(3, booking.getSeats());
+            ps.setDouble(4, booking.getTotalPrice());
+            ps.setString(5, booking.getStatus());
+            ps.setInt(6, booking.getUserId());
+            ps.setInt(7, booking.getFlightId());
+
+            return ps;
+
+        }, keyHolder);
+
+        return keyHolder.getKey().intValue();
 
     }
 
@@ -38,7 +52,24 @@ public class BookingDaoImpl implements BookingDao {
         String sql = "SELECT * FROM booking WHERE user_id=?";
 
         return jdbcTemplate.query(sql,
-                new BeanPropertyRowMapper<>(Booking.class),
+
+                (rs, rowNum) -> {
+
+                    Booking booking = new Booking();
+
+                    booking.setBookingId(rs.getInt("booking_id"));
+                    booking.setBookingDate(rs.getString("booking_date"));
+                    booking.setJourneyDate(rs.getString("journey_date"));
+                    booking.setSeats(rs.getInt("seats"));
+                    booking.setTotalPrice(rs.getDouble("total_price"));
+                    booking.setStatus(rs.getString("status"));
+                    booking.setUserId(rs.getInt("user_id"));
+                    booking.setFlightId(rs.getInt("flight_id"));
+
+                    return booking;
+
+                },
+
                 userId);
 
     }
