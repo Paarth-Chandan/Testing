f25f7a3497a33b1ac29f9d58c165d5470f1eccba
Paarth Chandan <135144621+Paarth-Chandan@users.noreply.github.com>
2026-08-05 07:11:50 +0530

    Update FlightController.java

--- FlightController.java
+++ FlightController.java
@@ -2,12 +2,9 @@ package com.flightreservation.controller;
 
 import java.util.List;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import com.flightreservation.model.Flight;
@@ -23,51 +20,21 @@ public class FlightController {
     @GetMapping("/search")
     public String searchPage() {
 
-        return "searchFlight";
+        return "flight/searchFlight";
+
     }
 
     @PostMapping("/search")
-    public String searchFlights(@RequestParam String source,
-                                @RequestParam String destination,
-                                Model model) {
+    public String searchFlight(@RequestParam String source,
+                               @RequestParam String destination,
+                               Model model) {
 
         List<Flight> flights =
                 flightService.searchFlights(source, destination);
 
         model.addAttribute("flights", flights);
 
-        return "searchFlight";
-    }
-
-    @GetMapping("/view")
-    public String viewFlights(Model model) {
-
-        model.addAttribute("flights",
-                flightService.getAllFlights());
-
-        return "viewFlights";
-    }
-
-    @GetMapping("/add")
-    public String addFlightPage(Model model) {
-
-        model.addAttribute("flight", new Flight());
-
-        return "addFlight";
-    }
-
-    @PostMapping("/add")
-    public String addFlight(@Valid @ModelAttribute Flight flight,
-                            BindingResult result) {
-
-        if(result.hasErrors()) {
-
-            return "addFlight";
-        }
-
-        flightService.addFlight(flight);
-
-        return "redirect:/flight/view";
+        return "flight/searchFlight";
     }
 
 }
