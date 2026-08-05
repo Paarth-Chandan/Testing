9e72ba4e61390e87ddb214534f0fdeeecec81f50
Paarth Chandan <135144621+Paarth-Chandan@users.noreply.github.com>
2026-08-05 07:11:28 +0530

    Update AdminController.java

--- AdminController.java
+++ AdminController.java
@@ -20,7 +20,6 @@ public class AdminController {
 
     @GetMapping("/dashboard")
     public String dashboard() {
-
         return "admin/adminDashboard";
     }
 
@@ -33,11 +32,10 @@ public class AdminController {
     }
 
     @PostMapping("/addFlight")
-    public String addFlight(@Valid @ModelAttribute Flight flight,
+    public String addFlight(@Valid @ModelAttribute("flight") Flight flight,
                             BindingResult result) {
 
         if(result.hasErrors()) {
-
             return "admin/addFlight";
         }
 
@@ -49,10 +47,35 @@ public class AdminController {
     @GetMapping("/viewFlights")
     public String viewFlights(Model model) {
 
-        model.addAttribute("flights",
-                flightService.getAllFlights());
+        model.addAttribute("flights", flightService.getAllFlights());
 
         return "admin/viewFlights";
+    }
+
+    @GetMapping("/editFlight/{id}")
+    public String editFlight(@PathVariable("id") int id,
+                             Model model) {
+
+        model.addAttribute("flight",
+                flightService.getFlightById(id));
+
+        return "admin/editFlight";
+    }
+
+    @PostMapping("/updateFlight")
+    public String updateFlight(@ModelAttribute Flight flight) {
+
+        flightService.updateFlight(flight);
+
+        return "redirect:/admin/viewFlights";
+    }
+
+    @GetMapping("/deleteFlight/{id}")
+    public String deleteFlight(@PathVariable("id") int id) {
+
+        flightService.deleteFlight(id);
+
+        return "redirect:/admin/viewFlights";
     }
 
 }
