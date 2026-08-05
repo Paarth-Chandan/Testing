e2863f62fed02ba40a76568224de407f299124be
Paarth Chandan <135144621+Paarth-Chandan@users.noreply.github.com>
2026-08-05 06:51:42 +0530

    Update FlightController.java

--- FlightController.java
+++ FlightController.java
@@ -2,10 +2,14 @@ package com.flightreservation.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
 import com.flightreservation.model.Flight;
 import com.flightreservation.service.FlightService;
 
@@ -20,13 +24,12 @@ public class FlightController {
     public String searchPage() {
 
         return "searchFlight";
-
     }
 
     @PostMapping("/search")
-    public String search(@RequestParam String source,
-                         @RequestParam String destination,
-                         Model model) {
+    public String searchFlights(@RequestParam String source,
+                                @RequestParam String destination,
+                                Model model) {
 
         List<Flight> flights =
                 flightService.searchFlights(source, destination);
@@ -34,7 +37,6 @@ public class FlightController {
         model.addAttribute("flights", flights);
 
         return "searchFlight";
-
     }
 
     @GetMapping("/view")
@@ -44,7 +46,28 @@ public class FlightController {
                 flightService.getAllFlights());
 
         return "viewFlights";
+    }
 
+    @GetMapping("/add")
+    public String addFlightPage(Model model) {
+
+        model.addAttribute("flight", new Flight());
+
+        return "addFlight";
+    }
+
+    @PostMapping("/add")
+    public String addFlight(@Valid @ModelAttribute Flight flight,
+                            BindingResult result) {
+
+        if(result.hasErrors()) {
+
+            return "addFlight";
+        }
+
+        flightService.addFlight(flight);
+
+        return "redirect:/flight/view";
     }
 
 }
